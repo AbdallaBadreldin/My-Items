d482f93d04924817007605aa1cc24d4a91fb42c9
Abdalla badr <abdalla.badr852@gmail.com>
2024-07-16 15:22:52 +0300

    Update HomeScreen.kt

--- MyItems/app/src/main/java/com/fstech/myItems/HomeScreen.kt
+++ MyItems/app/src/main/java/com/fstech/myItems/HomeScreen.kt
@@ -1,56 +1,46 @@
 package com.fstech.myItems
 
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.verticalScroll
 import androidx.compose.material3.Button
-import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
-import com.fstech.myItems.ui.theme.MyItemsTheme
 
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement =  Arrangement.SpaceEvenly,
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxSize()
+            .verticalScroll(rememberScrollState())
+    ) {
+        Text(text = "Pickup a Service")
 
-    MyItemsTheme {
-        Scaffold(modifier = Modifier.fillMaxSize().verticalScroll(rememberScrollState())) { innerPadding ->
-        Column {
-            Text(text = "Pickup a Service")
-            Button(onClick = { navController.navigate(Screen.FoundItemScreen.route) }) {
-                Text(text = "I Found Item")
-            }
-
-            Button(onClick = { navController.navigate(Screen.LostItemScreen.route) }) {
-            Text(text = "I Lost Item")
-            }
-
+        Button(onClick = { navController.navigate(Screen.FoundItemScreen.route) }) {
+            Text(text = "I Found An Item")
         }
-            Text(text = "Android", modifier = Modifier
-                .padding(innerPadding)
-                .clickable {
-                    navController.navigate(Screen.BakingScreen.route) {
-                        popUpTo(Screen.MainScreen.route) {
-                            inclusive = true
-                        }
-                    }
-                })
+
+        Button(onClick = { navController.navigate(Screen.LostItemScreen.route) }) {
+            Text(text = "I Lost An Item")
         }
-    }
-
-
-    @Composable
-    fun Greeting(name: String, modifier: Modifier = Modifier) {
-        Text(
-            text = "Hello $name!",
-            modifier = modifier.clickable {
-
-            },
-        )
+        Button(onClick = { navController.navigate(Screen.MapScreen.route) }) {
+            Text(text = "Search Items Casually")
+        }
     }
 
 }
+
+
+
