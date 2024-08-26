ed5f727a9360d64ea75581c1444f217d1f7a0b1b
Abdalla badr <abdalla.badr852@gmail.com>
2024-08-26 11:28:47 +0300

    update versions

--- MyItems/app/build.gradle.kts
+++ MyItems/app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         applicationId = "com.fstech.myItems"
         minSdk = 23
         targetSdk = 35
-        versionCode = 4
+        versionCode = 6
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
